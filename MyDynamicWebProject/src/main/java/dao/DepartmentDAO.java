151db17243de06c5d2f751d585ffc2eb67ccce72
aryan1731 <108079469+aryan1731@users.noreply.github.com>
2023-08-02 18:08:07 +0530

    Add files via upload

---
+++ MyDynamicWebProject/src/main/java/dao/DepartmentDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+//import entities;
+
+import java.util.List;
+
+import entities.Department;
+
+//II. POJI
+public interface DepartmentDAO { //POJI as per the POJO
+
+	public void insertDepartment(Department dept); //C
+	public Department selectDepartment(int deptNumber);//R
+	public List<entities.Department> selectDepartments(); //RA
+	public void updateDepartment(Department dept); //U
+	public void deleteDepartment(int deptNumber);  //D
+	
+}
